683ad17e943453c038bbcbcf898b1bf46d15cf60
Azzy <36964375+AzraelBunn@users.noreply.github.com>
2026-05-16 03:03:31 +0200

    fixed the whole mod

--- src/data-final-fixes.lua
+++ src/data-final-fixes.lua
@@ -2,7 +2,7 @@ data.raw.technology["quality-module"].effects ={{type = "unlock-recipe",recipe =
 data.raw.technology["epic-quality"].effects ={{type = "unlock-quality",quality = "normal"}}
 data.raw.technology["legendary-quality"].effects ={{type = "unlock-quality",quality = "normal"}}
 
-local tech = data.raw.technology["maraxsis-hydro-plant"]
+local tech = data.raw.technology["maraxsis-hydro-plant"].effects
 table.insert(tech, {type = "unlock-quality",quality = "uncommon"})
 table.insert(tech, {type = "unlock-quality",quality = "rare"})
 table.insert(tech, {type = "unlock-quality",quality = "epic"})
